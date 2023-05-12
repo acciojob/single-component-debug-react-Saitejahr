f124474c900716d2adbee2b871f2a6a39d61b309
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-05-12 15:56:41 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,11 @@
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <p>
+        Now I can render any React component on any DOM node I want using
+        ReactDOM.render
+      </p>
+    </div>
+  )
 }
 export default App
